bd14dc87c703e5d1876e9f2a29cfc6074e07cff2
Gregory M. Turner <gmturner007@ameritech.net>
2012-08-19 13:22:02 -0700

    Add rebasing scripts
    I use the scripts frequently to deal with rebasing problems.
    Since without a full rebasing solution, cygwin-portage is not
    particularly useful, I have included them here.
    
    I have plans to improve things so that this rebasing step will
    not be neccesary, but for now this is what does the trick for me.
    
    Signed-off-by: Gregory M. Turner <gmturner007@ameritech.net>

---
+++ scripts/pfxrebase.bat
@@ -0,0 +1,15 @@
+C:
+
+REM "Note: The following variables must be customized for your system"
+set CYGBINDIR_WIN=C:\cygwin\bin
+set CYGBINDIR=/bin
+set PFXDIR=/g2pfx
+set OVERLAYDIR=/g2pfx/overlay
+
+cd %CYGBINDIR_WIN%
+
+REM "%CYGBINDIR_WIN%\ash.exe" -c "PATH=%CYGBINDIR% %CYGBINDIR%/rebaseall"
+"%CYGBINDIR_WIN%\ash.exe" -c "PATH=%CYGBINDIR% %OVERLAYDIR%/scripts/rebaseall_pfx -P %PFXDIR%"
+"%CYGBINDIR_WIN%\ash.exe" -c "PATH=%CYGBINDIR% %OVERLAYDIR%/scripts/peflagsall_pfx -P %PFXDIR%"
+
+pause
