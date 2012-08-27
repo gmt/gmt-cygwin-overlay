412f5e7e70f151a3029311b271b247267688a267
Gregory M. Turner <gmturner007@ameritech.net>
2012-08-27 03:13:03 -0700

    scripts/rebase: The beginning of the end of rebasing problems!
    This time I got python to work.
    
    o Extend the determine_new_base and force_reasonable_base functions
      to support upward and downward growth
    o Add beautiful ASCII-art
    o Long overdue whitespace reconditioning
    o Bring back python support, and base it low.
    o Fuck yeah!  It works, so far, at least, for me.  Welcome to the
      era of gentoo-cygwin once again not sucking ginormous donkey
      penis!  May it last a few weeks or longer.
    
    Signed-off-by: Gregory M. Turner <gmturner007@ameritech.net>

--- scripts/pfxrebase.bat
+++ scripts/pfxrebase.bat
@@ -6,11 +6,8 @@ set CYGBINDIR=/bin
 set PFXDIR=/g2pfx
 set OVERLAYDIR=/g2pfx/overlay
 
-set DEBUG_REBASEALL_PFX=1
-
 cd %CYGBINDIR_WIN%
 
-REM "%CYGBINDIR_WIN%\ash.exe" -c "PATH=%CYGBINDIR% %CYGBINDIR%/rebaseall"
 "%CYGBINDIR_WIN%\ash.exe" -c "PATH=%CYGBINDIR% %OVERLAYDIR%/scripts/rebaseall_pfx -P %PFXDIR%"
 "%CYGBINDIR_WIN%\ash.exe" -c "PATH=%CYGBINDIR% %OVERLAYDIR%/scripts/peflagsall_pfx -P %PFXDIR%"
 
