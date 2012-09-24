1894a6e62b32313940f0c93280237e70ebd33c8f
Gregory M. Turner <gmturner007@ameritech.net>
2012-09-24 03:45:05 -0700

    scripts/pfxrebase.bat: Always nuke
    This just seemed more sensible, if you can call it that.
    I'm still not very happy with how this all works, but it's
    been working much better than before.  The effect of this
    change is probably pretty negligible, all things conisdered.
    
    Signed-off-by: Gregory M. Turner <gmturner007@ameritech.net>

--- scripts/pfxrebase.bat
+++ scripts/pfxrebase.bat
@@ -8,7 +8,7 @@ set OVERLAYDIR=/g2pfx/overlay
 
 cd %CYGBINDIR_WIN%
 
-"%CYGBINDIR_WIN%\ash.exe" -c "PATH=%CYGBINDIR% %OVERLAYDIR%/scripts/rebaseall_pfx -P %PFXDIR%"
+"%CYGBINDIR_WIN%\ash.exe" -c "PATH=%CYGBINDIR% %OVERLAYDIR%/scripts/rebaseall_pfx -P %PFXDIR% -N"
 "%CYGBINDIR_WIN%\ash.exe" -c "PATH=%CYGBINDIR% %OVERLAYDIR%/scripts/peflagsall_pfx -P %PFXDIR%"
 
 pause
